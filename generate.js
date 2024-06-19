96baad6cdcf6bd5d4bde52a72d4836dea04dba8d
kacperwalter <kacwal@poczta.fm>
2024-06-19 14:53:46 +0200

    Script now generates one HTML file

--- generate.js
+++ generate.js
@@ -1,37 +1,39 @@
-const fs = require('fs-extra')
-const handlebars = require('handlebars')
-const sass = require('node-sass')
+const fs = require('fs-extra');
+const handlebars = require('handlebars');
+const sass = require('node-sass');
 
+// Funkcja kompilująca SCSS do CSS
 function compileSass() {
     return new Promise((resolve, reject) => {
         sass.render({
             file: './styles/style.scss',
             outputStyle: 'compressed'
         }, (err, result) => {
-            if (err) reject(err)
-            else {
-                fs.outputFile('./dist/style.css', result.css, resolve)
-            }
-        })
-    })
+            if (err) reject(err);
+            else resolve(result.css.toString());
+        });
+    });
 }
 
-function generateHtml() {
-    const template = fs.readFileSync('./templates/landing.hbs', 'utf-8')
-    const templateCompile = handlebars.compile(template)
-    const data = fs.readJsonSync('./data/content.json')
-    const html = templateCompile(data)
-    fs.outputFileSync('./dist/index.html', html)
-}
-
-async function buildSite() {
+// Funkcja generująca HTML
+async function generateHtml() {
     try {
-        await compileSass()
-        generateHtml()
-        console.log('Site generated successfully!')
+        const css = await compileSass();  // Czekamy na skompilowany CSS
+        const template = fs.readFileSync('./templates/landing.hbs', 'utf-8');
+        const templateCompile = handlebars.compile(template);
+        const data = fs.readJsonSync('./data/content.json');
+
+        // Dodajemy CSS bezpośrednio do HTML
+        const html = templateCompile({
+            ...data,
+            style: css
+        });
+
+        fs.outputFileSync('./dist/index.html', html);
+        console.log('Site generated successfully with inline CSS!');
     } catch (error) {
-        console.error('Failed to build site:', error)
+        console.error('Failed to generate HTML:', error);
     }
 }
 
-buildSite()
+generateHtml();  // Uruchomienie funkcji generującej HTML
